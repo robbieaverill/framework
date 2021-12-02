c0d1927a636490ac8dbaf6feca40b0c155ae5baa
Dries Vints <dries@vints.io>
2021-12-02 21:01:15 +0100

    wip

--- tests/Integration/Database/QueryBuilderTest.php
+++ tests/Integration/Database/QueryBuilderTest.php
@@ -123,103 +123,103 @@ class QueryBuilderTest extends DatabaseTestCase
         $this->assertTrue(DB::table('posts')->where($subQuery, '!=', 'Does not match')->exists());
     }
 
-    // public function testWhereValueSubQueryBuilder()
-    // {
-    //     $subQuery = DB::table('posts')->selectRaw("'Sub query value'")->limit(1);
+    public function testWhereValueSubQueryBuilder()
+    {
+        $subQuery = DB::table('posts')->selectRaw("'Sub query value'")->limit(1);
 
-    //     $this->assertTrue(DB::table('posts')->where($subQuery, 'Sub query value')->exists());
-    //     $this->assertFalse(DB::table('posts')->where($subQuery, 'Does not match')->exists());
-    //     $this->assertTrue(DB::table('posts')->where($subQuery, '!=', 'Does not match')->exists());
-    // }
+        $this->assertTrue(DB::table('posts')->where($subQuery, 'Sub query value')->exists());
+        $this->assertFalse(DB::table('posts')->where($subQuery, 'Does not match')->exists());
+        $this->assertTrue(DB::table('posts')->where($subQuery, '!=', 'Does not match')->exists());
+    }
 
-    // public function testWhereDate()
-    // {
-    //     $this->assertSame(1, DB::table('posts')->whereDate('created_at', '2018-01-02')->count());
-    //     $this->assertSame(1, DB::table('posts')->whereDate('created_at', new Carbon('2018-01-02'))->count());
-    // }
+    public function testWhereDate()
+    {
+        $this->assertSame(1, DB::table('posts')->whereDate('created_at', '2018-01-02')->count());
+        $this->assertSame(1, DB::table('posts')->whereDate('created_at', new Carbon('2018-01-02'))->count());
+    }
 
-    // public function testOrWhereDate()
-    // {
-    //     $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereDate('created_at', '2018-01-02')->count());
-    //     $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereDate('created_at', new Carbon('2018-01-02'))->count());
-    // }
+    public function testOrWhereDate()
+    {
+        $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereDate('created_at', '2018-01-02')->count());
+        $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereDate('created_at', new Carbon('2018-01-02'))->count());
+    }
 
-    // public function testWhereDay()
-    // {
-    //     $this->assertSame(1, DB::table('posts')->whereDay('created_at', '02')->count());
-    //     $this->assertSame(1, DB::table('posts')->whereDay('created_at', 2)->count());
-    //     $this->assertSame(1, DB::table('posts')->whereDay('created_at', new Carbon('2018-01-02'))->count());
-    // }
+    public function testWhereDay()
+    {
+        $this->assertSame(1, DB::table('posts')->whereDay('created_at', '02')->count());
+        $this->assertSame(1, DB::table('posts')->whereDay('created_at', 2)->count());
+        $this->assertSame(1, DB::table('posts')->whereDay('created_at', new Carbon('2018-01-02'))->count());
+    }
 
-    // public function testOrWhereDay()
-    // {
-    //     $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereDay('created_at', '02')->count());
-    //     $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereDay('created_at', 2)->count());
-    //     $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereDay('created_at', new Carbon('2018-01-02'))->count());
-    // }
+    public function testOrWhereDay()
+    {
+        $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereDay('created_at', '02')->count());
+        $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereDay('created_at', 2)->count());
+        $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereDay('created_at', new Carbon('2018-01-02'))->count());
+    }
 
-    // public function testWhereMonth()
-    // {
-    //     $this->assertSame(1, DB::table('posts')->whereMonth('created_at', '01')->count());
-    //     $this->assertSame(1, DB::table('posts')->whereMonth('created_at', 1)->count());
-    //     $this->assertSame(1, DB::table('posts')->whereMonth('created_at', new Carbon('2018-01-02'))->count());
-    // }
+    public function testWhereMonth()
+    {
+        $this->assertSame(1, DB::table('posts')->whereMonth('created_at', '01')->count());
+        $this->assertSame(1, DB::table('posts')->whereMonth('created_at', 1)->count());
+        $this->assertSame(1, DB::table('posts')->whereMonth('created_at', new Carbon('2018-01-02'))->count());
+    }
 
-    // public function testOrWhereMonth()
-    // {
-    //     $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereMonth('created_at', '01')->count());
-    //     $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereMonth('created_at', 1)->count());
-    //     $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereMonth('created_at', new Carbon('2018-01-02'))->count());
-    // }
+    public function testOrWhereMonth()
+    {
+        $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereMonth('created_at', '01')->count());
+        $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereMonth('created_at', 1)->count());
+        $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereMonth('created_at', new Carbon('2018-01-02'))->count());
+    }
 
-    // public function testWhereYear()
-    // {
-    //     $this->assertSame(1, DB::table('posts')->whereYear('created_at', '2018')->count());
-    //     $this->assertSame(1, DB::table('posts')->whereYear('created_at', 2018)->count());
-    //     $this->assertSame(1, DB::table('posts')->whereYear('created_at', new Carbon('2018-01-02'))->count());
-    // }
+    public function testWhereYear()
+    {
+        $this->assertSame(1, DB::table('posts')->whereYear('created_at', '2018')->count());
+        $this->assertSame(1, DB::table('posts')->whereYear('created_at', 2018)->count());
+        $this->assertSame(1, DB::table('posts')->whereYear('created_at', new Carbon('2018-01-02'))->count());
+    }
 
-    // public function testOrWhereYear()
-    // {
-    //     $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereYear('created_at', '2018')->count());
-    //     $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereYear('created_at', 2018)->count());
-    //     $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereYear('created_at', new Carbon('2018-01-02'))->count());
-    // }
+    public function testOrWhereYear()
+    {
+        $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereYear('created_at', '2018')->count());
+        $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereYear('created_at', 2018)->count());
+        $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereYear('created_at', new Carbon('2018-01-02'))->count());
+    }
 
-    // public function testWhereTime()
-    // {
-    //     $this->assertSame(1, DB::table('posts')->whereTime('created_at', '03:04:05')->count());
-    //     $this->assertSame(1, DB::table('posts')->whereTime('created_at', new Carbon('2018-01-02 03:04:05'))->count());
-    // }
+    public function testWhereTime()
+    {
+        $this->assertSame(1, DB::table('posts')->whereTime('created_at', '03:04:05')->count());
+        $this->assertSame(1, DB::table('posts')->whereTime('created_at', new Carbon('2018-01-02 03:04:05'))->count());
+    }
 
-    // public function testOrWhereTime()
-    // {
-    //     $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereTime('created_at', '03:04:05')->count());
-    //     $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereTime('created_at', new Carbon('2018-01-02 03:04:05'))->count());
-    // }
+    public function testOrWhereTime()
+    {
+        $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereTime('created_at', '03:04:05')->count());
+        $this->assertSame(2, DB::table('posts')->where('id', 1)->orWhereTime('created_at', new Carbon('2018-01-02 03:04:05'))->count());
+    }
 
-    // public function testPaginateWithSpecificColumns()
-    // {
-    //     $result = DB::table('posts')->paginate(5, ['title', 'content']);
+    public function testPaginateWithSpecificColumns()
+    {
+        $result = DB::table('posts')->paginate(5, ['title', 'content']);
 
-    //     $this->assertInstanceOf(LengthAwarePaginator::class, $result);
-    //     $this->assertEquals($result->items(), [
-    //         (object) ['title' => 'Foo Post', 'content' => 'Lorem Ipsum.'],
-    //         (object) ['title' => 'Bar Post', 'content' => 'Lorem Ipsum.'],
-    //     ]);
-    // }
+        $this->assertInstanceOf(LengthAwarePaginator::class, $result);
+        $this->assertEquals($result->items(), [
+            (object) ['title' => 'Foo Post', 'content' => 'Lorem Ipsum.'],
+            (object) ['title' => 'Bar Post', 'content' => 'Lorem Ipsum.'],
+        ]);
+    }
 
-    // public function testChunkMap()
-    // {
-    //     DB::enableQueryLog();
+    public function testChunkMap()
+    {
+        DB::enableQueryLog();
 
-    //     $results = DB::table('posts')->orderBy('id')->chunkMap(function ($post) {
-    //         return $post->title;
-    //     }, 1);
+        $results = DB::table('posts')->orderBy('id')->chunkMap(function ($post) {
+            return $post->title;
+        }, 1);
 
-    //     $this->assertCount(2, $results);
-    //     $this->assertSame('Foo Post', $results[0]);
-    //     $this->assertSame('Bar Post', $results[1]);
-    //     $this->assertCount(3, DB::getQueryLog());
-    // }
+        $this->assertCount(2, $results);
+        $this->assertSame('Foo Post', $results[0]);
+        $this->assertSame('Bar Post', $results[1]);
+        $this->assertCount(3, DB::getQueryLog());
+    }
 }
